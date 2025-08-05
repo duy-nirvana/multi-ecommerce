89cafd5a14526b19a4a3d8885bea8e969fa1e050
doc_co_cau_bai <duytran@gleads.vn>
2025-08-05 16:58:19 +0700

    update ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Hello world</Button>
+    <div className="p-6">
+      <div className="flex flex-col gap-4">
+        <div>
+          <Button variant="elevated">Hello world</Button>
+        </div>
+        <div>
+          <Input placeholder="I am an input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="I am a textarea" />
+        </div>
+        <div>
+          <Checkbox />
+        </div>
+      </div>
     </div>
   );
 }
