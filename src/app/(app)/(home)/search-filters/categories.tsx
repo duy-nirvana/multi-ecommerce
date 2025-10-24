11ae1d6d9319baffcf40c55a4588afe72510e64b
duytk <duy.trankhanh@mercurystudiogroup.com>
2025-10-24 09:04:25 +0700

    Add UI cate

--- src/app/(app)/(home)/search-filters/categories.tsx
+++ src/app/(app)/(home)/search-filters/categories.tsx
@@ -1,11 +1,18 @@
 import CategoryDropdown from "./category-dropdown";
 import { CustomCategory } from "../types";
+import { useRef, useState } from "react";
 
 interface Props {
   data: CustomCategory[];
 }
 
 export const Categories = ({ data }: Props) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const measureRef = useRef<HTMLDivElement>(null);
+  const viewAllRef = useRef<HTMLDivElement>(null);
+
+  const [visibleCount, setVisibleCount] = useState(data.length);
+
   return (
     <div className="relative w-full">
       <div className="flex flex-nowrap items-center">
